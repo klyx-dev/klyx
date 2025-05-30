757179073c238ed3c8d4ec11df691fecb9e1e8c1
Vivek <itsvks19@gmail.com>
2025-05-30 15:27:53 +0530

    build: bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.klyx"
         minSdk = 26
         targetSdk = 36
-        versionCode = 100
-        versionName = "1.0.0"
+        versionCode = 101
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
