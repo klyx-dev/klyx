a35dd18e84e2dfb5bc4228ad34f334bea8993486
Vivek <itsvks19@gmail.com>
2025-06-01 16:03:50 +0530

    chore(release): v1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.klyx"
         minSdk = 26
         targetSdk = 36
-        versionCode = 110
-        versionName = "1.1.0"
+        versionCode = 120
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
