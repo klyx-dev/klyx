bc7f11819095e897323c26d745e9c3b1645193b5
Vivek <itsvks19@gmail.com>
2025-06-02 11:42:14 +0530

    feat(extension): add `create_file` function to `FileSystem`

--- extension-api/src/main/java/com/klyx/extension/impl/FileSystem.kt
+++ extension-api/src/main/java/com/klyx/extension/impl/FileSystem.kt
@@ -2,15 +2,18 @@ package com.klyx.extension.impl
 
 import com.klyx.extension.ExtensionHostModule
 import com.klyx.extension.HostFunctionDefinition
+import com.klyx.extension.wasm.toHostFunction
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class FileSystem : ExtensionHostModule {
     override val namespace get() = "FileSystem"
 
-    override fun getHostFunctions(): List<HostFunctionDefinition> {
-        return listOf()
-    }
+    override fun getHostFunctions() = listOf(
+        HostFunctionDefinition("create_file", ::createFile.toHostFunction())
+    )
 
     private fun createFile(path: String) {
-
+        Files.createFile(Paths.get(path))
     }
 }
