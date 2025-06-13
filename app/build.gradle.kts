c650e45a198a4e55ffeb651544c11fc240cf004a
Vivek <itsvks19@gmail.com>
2025-06-13 14:48:22 +0530

    chore(release): v1.3.0-beta.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.klyx"
         minSdk = 26
         targetSdk = 36
-        versionName = "1.3.0-alpha.3"
+        versionName = "1.3.0-beta.0"
         versionCode = calculateVersionCode(versionName!!)
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
