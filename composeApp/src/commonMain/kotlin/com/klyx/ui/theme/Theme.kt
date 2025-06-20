90fc14f0d89a7cfb0410e2922b8d598e3bbafe4c
Vivek <itsvks19@gmail.com>
2025-06-20 16:01:04 +0530

    refactor(theme): replace MaterialTheme with MaterialExpressiveTheme

--- composeApp/src/commonMain/kotlin/com/klyx/ui/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/klyx/ui/theme/Theme.kt
@@ -3,7 +3,7 @@ package com.klyx.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -275,7 +275,7 @@ fun KlyxTheme(
         else -> lightScheme
     }
 
-    MaterialTheme(
+    MaterialExpressiveTheme(
         colorScheme = colorScheme,
         typography = AppTypography,
         content = content
--- core/src/commonMain/kotlin/com/klyx/core/Encoding.kt
+++ core/src/commonMain/kotlin/com/klyx/core/Encoding.kt
@@ -1,4 +1,3 @@
 package com.klyx.core
 
 expect fun decodeBase64(base64: String): ByteArray
-
