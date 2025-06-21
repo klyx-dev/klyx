9dac0eb757d5ed6fd7e4453c9a41dd8c1f6c2eab
Vivek <itsvks19@gmail.com>
2025-06-21 10:10:52 +0530

    feat(command): implement platform-specific CommandManager for Android and JVM

---
+++ core/src/androidMain/kotlin/com/klyx/core/cmd/CommandManager.android.kt
@@ -0,0 +1,73 @@
+package com.klyx.core.cmd
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateSetOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.type
+import com.klyx.core.cmd.key.ShortcutSequence
+import com.klyx.core.cmd.key.matchesSequence
+import com.klyx.core.cmd.key.parseShortcut
+import com.klyx.core.event.EventBus
+import com.klyx.core.theme.ThemeManager
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual object CommandManager {
+    actual val commands = mutableStateListOf<Command>()
+    actual val recentlyUsedCommands = mutableStateSetOf<Command>()
+
+    private val activeSequences = mutableMapOf<Command, ShortcutSequence>()
+
+    actual var showCommandPalette by mutableStateOf(false)
+
+    init {
+        EventBus.instance.subscribe<KeyEvent> { event ->
+            if (event.type == KeyEventType.KeyDown) {
+                activeSequences.entries.removeIf { (command, sequence) ->
+                    if (event.matchesSequence(sequence)) {
+                        if (sequence.advance()) {
+                            command.execute(command)
+                            true
+                        } else false
+                    } else true
+                }
+
+                for (command in commands) {
+                    command.shortcutKey?.let { key ->
+                        val sequence = parseShortcut(key)
+                        if (event.matchesSequence(sequence)) {
+                            if (sequence.advance()) {
+                                command.execute(command)
+                            } else {
+                                activeSequences[command] = sequence
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        addCommand(Command("Toggle theme selector", shortcutKey = "Ctrl-K Ctrl-T") {
+            ThemeManager.toggleThemeSelector()
+        })
+    }
+
+    actual fun showPalette() {
+        showCommandPalette = true
+    }
+
+    actual fun hidePalette() {
+        showCommandPalette = false
+    }
+
+    actual fun addCommand(vararg command: Command) {
+        commands.addAll(command)
+    }
+
+    actual fun addRecentlyUsedCommand(command: Command) {
+        recentlyUsedCommands.add(command)
+    }
+}
--- core/src/commonMain/kotlin/com/klyx/core/cmd/CommandManager.kt
+++ core/src/commonMain/kotlin/com/klyx/core/cmd/CommandManager.kt
@@ -1,72 +1,21 @@
 package com.klyx.core.cmd
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.mutableStateSetOf
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.input.key.KeyEvent
-import androidx.compose.ui.input.key.KeyEventType
-import androidx.compose.ui.input.key.type
-import com.klyx.core.cmd.key.ShortcutSequence
-import com.klyx.core.cmd.key.matchesSequence
-import com.klyx.core.cmd.key.parseShortcut
-import com.klyx.core.event.EventBus
-import com.klyx.core.theme.ThemeManager
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.snapshots.SnapshotStateSet
 
-object CommandManager {
-    val commands = mutableStateListOf<Command>()
-    val recentlyUsedCommands = mutableStateSetOf<Command>()
-    private val activeSequences = mutableMapOf<Command, ShortcutSequence>()
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+expect object CommandManager {
+    val commands: SnapshotStateList<Command>
+    val recentlyUsedCommands: SnapshotStateSet<Command>
 
-    var showCommandPalette by mutableStateOf(false)
+    var showCommandPalette: Boolean
         private set
 
-    init {
-        EventBus.instance.subscribe<KeyEvent> { event ->
-            if (event.type == KeyEventType.KeyDown) {
-                activeSequences.entries.removeIf { (command, sequence) ->
-                    if (event.matchesSequence(sequence)) {
-                        if (sequence.advance()) {
-                            command.execute(command)
-                            true
-                        } else false
-                    } else true
-                }
+    fun showPalette()
 
-                for (command in commands) {
-                    command.shortcutKey?.let { key ->
-                        val sequence = parseShortcut(key)
-                        if (event.matchesSequence(sequence)) {
-                            if (sequence.advance()) {
-                                command.execute(command)
-                            } else {
-                                activeSequences[command] = sequence
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    fun hidePalette()
 
-        addCommand(Command("Toggle theme selector", shortcutKey = "Ctrl-K Ctrl-T") {
-            ThemeManager.toggleThemeSelector()
-        })
-    }
+    fun addCommand(vararg command: Command)
 
-    fun showPalette() {
-        showCommandPalette = true
-    }
-
-    fun hidePalette() {
-        showCommandPalette = false
-    }
-
-    fun addCommand(vararg command: Command) {
-        commands.addAll(command)
-    }
-
-    fun addRecentlyUsedCommand(command: Command) {
-        recentlyUsedCommands.add(command)
-    }
+    fun addRecentlyUsedCommand(command: Command)
 }
