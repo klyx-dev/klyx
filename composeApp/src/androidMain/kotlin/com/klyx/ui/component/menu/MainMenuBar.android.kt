3a679c94fa8211aeea7dc47af63ab33e586a7d37
Vivek <itsvks19@gmail.com>
2025-07-11 17:15:40 +0530

    fix: apply status bar padding to menu bar

--- composeApp/src/androidMain/kotlin/com/klyx/ui/component/menu/MainMenuBar.android.kt
+++ composeApp/src/androidMain/kotlin/com/klyx/ui/component/menu/MainMenuBar.android.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material3.Icon
@@ -312,7 +313,8 @@ actual fun MainMenuBar(modifier: Modifier) {
                     PopupMenu(
                         items = items,
                         position = menuItemPositions[rowTitle] ?: IntOffset.Zero,
-                        onDismissRequest = onDismissRequest
+                        onDismissRequest = onDismissRequest,
+                        modifier = Modifier.statusBarsPadding()
                     )
                 }
             }
