51407ed043eb3c2f54cc0071e7013bf59a1add4f
Vivek <itsvks19@gmail.com>
2025-07-28 00:49:00 +0530

    fix(extension): allow unknown names in toml

--- core/src/commonMain/kotlin/com/klyx/core/extension/extension.toml.kt
+++ core/src/commonMain/kotlin/com/klyx/core/extension/extension.toml.kt
@@ -1,15 +1,22 @@
 package com.klyx.core.extension
 
 import com.akuleshov7.ktoml.Toml
+import com.akuleshov7.ktoml.TomlInputConfig
 import kotlinx.io.RawSource
 import kotlinx.io.buffered
 import kotlinx.io.readString
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 
-fun createToml(toml: ExtensionToml) = Toml.encodeToString(toml)
+private val toml = Toml(
+    inputConfig = TomlInputConfig(
+        ignoreUnknownNames = true
+    )
+)
+
+fun createToml(extensionToml: ExtensionToml) = toml.encodeToString(extensionToml)
 
 fun parseToml(source: RawSource): ExtensionToml {
     val buffered = source.buffered().peek()
-    return Toml.decodeFromString(buffered.readString())
+    return toml.decodeFromString(buffered.readString())
 }
