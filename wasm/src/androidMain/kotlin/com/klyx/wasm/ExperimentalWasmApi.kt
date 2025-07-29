48e7aa846e0af46d07fda30bcedcbd03b7a82d90
Vivek <itsvks19@gmail.com>
2025-07-29 13:35:51 +0530

    refactor: add annotation targets for experimental APIs

--- wasm/src/androidMain/kotlin/com/klyx/wasm/ExperimentalWasmApi.kt
+++ wasm/src/androidMain/kotlin/com/klyx/wasm/ExperimentalWasmApi.kt
@@ -2,4 +2,11 @@ package com.klyx.wasm
 
 @RequiresOptIn(message = "This API is experimental and may change in the future.")
 @Retention(AnnotationRetention.BINARY)
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.TYPEALIAS,
+    AnnotationTarget.VALUE_PARAMETER
+)
 annotation class ExperimentalWasmApi()
