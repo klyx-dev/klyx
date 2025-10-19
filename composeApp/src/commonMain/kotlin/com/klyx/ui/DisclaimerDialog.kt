7b966f8957739e71774415f84146926961afe902
Vivek <itsvks19@gmail.com>
2025-10-19 15:03:01 +0530

    refactor: use AlertDialog in DisclaimerDialog

--- composeApp/src/commonMain/kotlin/com/klyx/ui/DisclaimerDialog.kt
+++ composeApp/src/commonMain/kotlin/com/klyx/ui/DisclaimerDialog.kt
@@ -6,13 +6,16 @@ import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.HelpOutline
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.window.DialogProperties
-import com.klyx.core.ui.component.KlyxDialog
+import com.klyx.core.ui.component.ConfirmButton
+import com.klyx.core.ui.component.DismissButton
 import com.klyx.res.Res
 import com.klyx.res.disclaimer
 import com.klyx.res.exit
@@ -22,40 +25,23 @@ import com.klyx.ui.page.main.quitApp
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
-fun DisclaimerDialog(
-    onAccept: () -> Unit,
-) {
-    KlyxDialog(
+fun DisclaimerDialog(onAccept: () -> Unit) {
+    AlertDialog(
         onDismissRequest = { },
         properties = DialogProperties(
             dismissOnBackPress = false,
             dismissOnClickOutside = false
         ),
-        title = {
-            Text(stringResource(Res.string.important_notice))
-        },
-        icon = {
-            Icon(
-                Icons.AutoMirrored.Outlined.HelpOutline,
-                contentDescription = null
-            )
-        },
+        title = { Text(stringResource(Res.string.important_notice), textAlign = TextAlign.Center) },
+        icon = { Icon(Icons.AutoMirrored.Outlined.HelpOutline, contentDescription = null) },
         text = {
             Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
                 SelectionContainer {
-                    Text(stringResource(Res.string.disclaimer))
+                    Text(stringResource(Res.string.disclaimer), style = MaterialTheme.typography.bodyLarge)
                 }
             }
         },
-        confirmButton = {
-            TextButton(onClick = onAccept) {
-                Text(stringResource(Res.string.i_agree))
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = ::quitApp) {
-                Text(stringResource(Res.string.exit))
-            }
-        }
+        confirmButton = { ConfirmButton(stringResource(Res.string.i_agree), onClick = onAccept) },
+        dismissButton = { DismissButton(stringResource(Res.string.exit), ::quitApp) }
     )
 }
